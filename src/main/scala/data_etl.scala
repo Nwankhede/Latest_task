f5c8aea1e2af0956ac224dc8268e50e0c5d9012e
Nitin Wankhede <classyclicks7@gmail.com>
2024-02-19 01:19:09 +0530

    Performed deup delete 1

--- src/main/scala/data_etl.scala
+++ src/main/scala/data_etl.scala
@@ -7,104 +7,104 @@ import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructFi
 
 
 object data_etl {
-      val logger = Logger.getRootLogger
+  val logger = Logger.getRootLogger
 
-      def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-      val spark = SparkSession.builder()
-        .appName("SparkSalesAnalysis")
-        .master("local[*]")
-        .getOrCreate()
+    val spark = SparkSession.builder()
+      .appName("SparkSalesAnalysis")
+      .master("local[*]")
+      .getOrCreate()
 
-      try {
+    try {
 
-        val salesDF = readData(spark, "src/main/resources/sales_data.csv", salesSchema)
-        logger.debug("Sales data read successfully")
-        val productDF = readData(spark, "src/main/resources/product_data.csv", productSchema)
-        logger.debug("Product data read successfully")
+      val salesDF = readData(spark, "src/main/resources/sales_data.csv", salesSchema).get //.get fetches the data into
+      logger.debug("Sales data read successfully")
+      val productDF = readData(spark, "src/main/resources/product_data.csv", productSchema).get
+      logger.debug("Product data read successfully")
 
+      val joinedDF = salesDF.join(productDF, Seq("Product_ID"), "inner")
+      joinedDF.show()
 
-        val joinedDF = salesDF.join(productDF, Seq("Product_ID"), "inner")
-        joinedDF.show()
+      val deDup = joinedDF.dropDuplicates()
 
-        val deDup = joinedDF.dropDuplicates()
+      deDup.show()
 
-        deDup.show()
+      // Caching to store the joined dataframe in-memory for better performance
+      joinedDF.cache()
+      logger.debug("Caching the data..")
 
-        // Caching to store the joined dataframe in-memory for better performance
-        joinedDF.cache()
-        logger.debug("Caching the data..")
+      val productSalesDF = joinedDF
+        .withColumn("Total_Sales_Amount", col("Quantity") * col("Price"))
+        .groupBy("Product_ID", "Product_Name", "Category", "Unit_Price")
+        .agg(sum("Total_Sales_Amount").alias("Total_Sales_Amount"))
+      logger.debug(s"show data for product: ${productSalesDF.show()}")
 
-        val productSalesDF = joinedDF
-          .withColumn("Total_Sales_Amount", col("Quantity") * col("Price"))
-          .groupBy("Product_ID", "Product_Name", "Category", "Unit_Price")
-          .agg(sum("Total_Sales_Amount").alias("Total_Sales_Amount"))
-        logger.debug(s"show data for product: ${productSalesDF.show()}")
+      val categorySalesDF = productSalesDF
+        .groupBy("Category")
+        .agg(sum("Total_Sales_Amount").alias("Total_Sales_Amount"))
 
-        val categorySalesDF = productSalesDF
-          .groupBy("Category")
-          .agg(sum("Total_Sales_Amount").alias("Total_Sales_Amount"))
+      logger.debug(s"show data for category: ${categorySalesDF.show()}")
 
-        logger.debug(s"show data for category: ${categorySalesDF.show()}")
+      cleanOutput("src/main/output")
+      logger.debug("cleaned the output directory successfully")
+      productSalesDF.write.csv("src/main/output/product_sales")
+      categorySalesDF.write.csv("src/main/output/category_sales")
+      logger.debug("data written to output directory successfully")
 
-        cleanOutput("src/main/output")
-        logger.debug("cleaned the output directory successfully")
-        productSalesDF.write.csv("src/main/output/product_sales")
-        categorySalesDF.write.csv("src/main/output/category_sales")
-        logger.debug("data written to output directory successfully")
-
-      } catch {
-        case ex: Exception =>
-          println(s"An error occurred: ${ex.getMessage}")
-      } finally {
-        spark.stop()
-      }
+    } catch {
+      case ex: Exception =>
+        println(s"An error occurred: ${ex.getMessage}")
+    } finally {
+      spark.stop()
     }
+  }
 
-    val salesSchema = StructType(Seq(
-      StructField("Transaction_ID", StringType, nullable = false),
-      StructField("Product_ID", StringType, nullable = false),
-      StructField("Quantity", IntegerType, nullable = false),
-      StructField("Price", DoubleType, nullable = false)
-    ))
+  val salesSchema = StructType(Seq(
+    StructField("Transaction_ID", StringType, nullable = false),
+    StructField("Product_ID", StringType, nullable = false),
+    StructField("Quantity", IntegerType, nullable = false),
+    StructField("Price", DoubleType, nullable = false)
+  ))
 
-    val productSchema = StructType(Seq(
-      StructField("Product_ID", StringType, nullable = false),
-      StructField("Product_Name", StringType, nullable = false),
-      StructField("Category", StringType, nullable = false),
-      StructField("Unit_Price", DoubleType, nullable = false)
-    ))
+  val productSchema = StructType(Seq(
+    StructField("Product_ID", StringType, nullable = false),
+    StructField("Product_Name", StringType, nullable = false),
+    StructField("Category", StringType, nullable = false),
+    StructField("Unit_Price", DoubleType, nullable = false)
+  ))
 
-    def cleanOutput(directoryPath: String): Unit ={
-      val directory = new File(directoryPath)
+  def cleanOutput(directoryPath: String): Unit ={
+    val directory = new File(directoryPath)
 
-      if (directory.exists && directory.isDirectory){
-        val files = directory.listFiles()
+    if (directory.exists && directory.isDirectory){
+      val files = directory.listFiles()
 
-        if (files != null) {
-          // delete each file now
-          files.foreach(file =>
-            if (file.isFile) {file.delete()
+      if (files != null) {
+        // delete each file now
+        files.foreach(file =>
+          if (file.isFile) {file.delete()
             logger.debug(s"Deleted file: ${file.getAbsolutePath}")
           } else if (file.isDirectory) {
-              cleanOutput(file.getAbsolutePath)
-            }
-          )
+            cleanOutput(file.getAbsolutePath)
+          }
+        )
         directory.delete()
         logger.debug(s"Deleted directory: $directoryPath")
-        }
-        else {
-          logger.debug(s" directory does not exist or is not a directory: ${directoryPath}")
-        }
+      }
+      else {
+        logger.debug(s" directory does not exist or is not a directory: ${directoryPath}")
       }
     }
+  }
 
-    def readData(spark: SparkSession, filePath: String, schema: StructType): DataFrame = {
+  def readData(spark: SparkSession, filePath: String, schema: StructType): Option[DataFrame] = {
+    val filepathdir = new File(filePath)
+    if (filepathdir.exists()){
       val df = spark.read
         .option("header", "true")
         .schema(schema)
         .csv(filePath)
-
       val columnsWithNull = schema.filter(field => df.filter(col(field.name).isNull).count() > 0).map(_.name)
       if (columnsWithNull.nonEmpty) {
         throw new IllegalArgumentException(s"Null values found in columns: ${columnsWithNull.mkString(", ")}")
@@ -116,8 +116,16 @@ object data_etl {
         }
       }
 
-      df
+      Some(df) // options data frame has been created
+
     }
+    else {
+      logger.debug(s"filepath does not exist")
+      return None
+    }
+
+
+  }
 
 
 }
